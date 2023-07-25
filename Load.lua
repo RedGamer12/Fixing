20ae456a49d559390a7426bfed3c1fa1bf91d5d0
RedGamer12 <73015566+RedGamer12@users.noreply.github.com>
2023-07-25 14:32:24 +0700

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -13,7 +13,7 @@ return function(Type, PatchName)
     end
 
     -- // Vars
-    local PatchFormat = "https://raw.githubusercontent.com/Stefanuk12/Aiming/main/GamePatches/%s/%s.lua"
+    local PatchFormat = "https://raw.githubusercontent.com/RedGamer12/Fixing/main/GamePatches/%s/%s.lua"
 
     -- // Attempt to load a patch for the game
     local Success, _ = pcall(function()
